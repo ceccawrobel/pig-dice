d0255a5a71ac05c8c9c637f3169a30e7d6ffcfe2
cecca wrobel and Vilija Jozaitis <vilija@>
2019-07-08 20:45:38 -0700

    add js functionality to add numbers as rolled, add turn total to player score with hold button, and add branching condition for roll of 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 //business logic
-function Player(totalScore, currentTurn) {
+function Player(totalScore, turnRolls) {
   this.totalScore = totalScore;
-  this.currentTurn = currentTurn;
+  this.turnRolls = turnRolls;
 }
 
 Player.prototype.addScore = function() {
@@ -13,13 +13,40 @@ $(document).ready(function() {
 
 var player1 = new Player(0, []);
 var player2 = new Player(0, []);
+var turnTotal = 0;
+var roll = 0
 
   $("#rollP1").submit(function(event) {
     event.preventDefault();
-    var roll = Math.floor(Math.random() * 6 + 1);
-    $("#turnP1").append("<li>" + roll + "</li>");
-    
+    roll = Math.floor(Math.random() * 6 + 1);
+    if (roll === 1) {
+      turnTotal = 0;
+      $("#turnTotalP1").text(turnTotal);
+      player1.turnRolls = [];
+      $("#turnP1").text("");
+      alert("You rolled a 1! Turn is over.");
+    } else {
 
+    player1.turnRolls.push(roll);
+    $("#turnP1").append("<li>" + roll + "</li>");
+    turnTotal = turnTotal + roll;
+    $("#turnTotalP1").text(turnTotal);
+    console.log(player1.turnRolls);
+    }
   });
+
+  $("#holdP1").submit(function(event) {
+    event.preventDefault();
+    player1.totalScore += turnTotal;
+    $("#scoreP1").text(player1.totalScore);
+    turnTotal = 0;
+    $("#turnTotalP1").text(turnTotal);
+    player1.turnRolls = [];
+    $("#turnP1").text("");
+    console.log(player1.turnRolls);
+  });
+
+// //insert this in to ROLL code
+
 
 })
